4ae686519709c13fe3228cbd63735860ebf2b080
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-31 17:32:38 +0100

    Inseriti nella pagina show delle tecnologies dei link (se presenti) che riportano quali progetti hanno utilizzato quel tipo di tencologia. Altrimenti stampo un messaggio di cortesia

--- resources/views/admin/technologies/show.blade.php
+++ resources/views/admin/technologies/show.blade.php
@@ -7,6 +7,16 @@
 @section('content')
     <h1>Technology name: <span class="text-decoration-underline">{{ $technology->name }}</span></h1>
 
+    @if( $technology->projects->isNotEmpty() )
+        <h3 class="mt-3">Where was this technology used:</h3>
+        <ul>
+            @foreach ($technology->projects as $project)
+                <li><a href="{{ route('admin.projects.show', $project) }}">{{ $project->project_title }}</a></li>
+            @endforeach
+        </ul>
+    @else 
+        <h3 class="mt-3">No projects have used {{ $technology->name }} yet.</h3>
+    @endif
     {{-- Nav links --}}
     <div class="mt-5">
         <a href="{{ route('admin.technologies.index') }}" class="btn btn-secondary">Technology List</a>
